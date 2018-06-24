a35fc75e43568178702e85cf7b59e096f6e7cd2d
oxc <oxc@oxc.com>
2018-06-24 17:02:06 +0800

    up 事件系统

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-const count = require('./counter')
-console.log(count())
+const events = require('events')
+const util = require('util')
+
+const Person = function(name) {
+    this.name = name
+}
+util.inherits(Person, events.EventEmitter)
+
+const xiaoming = new Person('xiaoming')
+const lili = new Person('lili')
+const lucy = new Person('lucy')
+
+const person = [xiaoming, lili, lucy]
+
+person.forEach(item => {
+    item.on('speak', msg => {
+        console.log(item.name + ' said: ' + msg)
+    })
+})
+
+xiaoming.emit('speak', 'hi')
+lili.emit('speak', 'hi')
+lucy.emit('speak', 'hi')
+
+// const myEmitter = new events.EventEmitter()
+
+// myEmitter.on('someEvent', msg => {
+//     console.log(msg)
+// })
+
+// myEmitter.emit('someEvent', 'oxc')
